2df131028ac2a4124e647006a14abb088ddf38e9
gaor-jjj <kaurtamm1995@gmail.com>
2024-10-15 13:59:44 +0300

    Creating models

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const sequelize = require('./util/db')
+
+const models = require('./models/index');
+sequelize.models = models;
+
+sequelize
+    .sync()
+    .then(() => {
+        console.log('Tabelid on loodud')
+        app.listen(3002);
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 app.get('/', (req, res) => {
     res.json({ message: 'Web shop app' })
 })
-
-app.listen(3002);
